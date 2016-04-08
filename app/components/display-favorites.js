0636e89c858c361659a5d6c06db93d4b854aad3a
Daren Schaad <darenschaad@gmail.com>
2016-04-08 08:53:08 -0700

    Add working service to favorite questions

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favorite: Ember.inject.service(),
-
-  actions: {
-    addToFavorites(question) {
-      this.get('favorite').add(question);
-    }
-  }
 });
