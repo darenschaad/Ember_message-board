bf953beb09801fcc2578fcf139188036cdec0e2c
Daren Schaad <darenschaad@gmail.com>
2016-04-11 14:16:51 -0700

    Publishing to github pages

--- app/models/answer.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  content: DS.attr(),
-  author: DS.attr(),
-  time: DS.attr(),
-  question: DS.belongsTo('question', { async: true })
-});
